9c5283bfcb5b8b9d9f1c673657ac5fae5a97960c
Daniel Martinez <daniel.martinez1015@gmail.com>
2017-11-22 16:03:35 -0800

    selectEmployee()
    commit

--- ReinTechTimeManagement/src/java/employee/data/EmployeeDB.java
+++ ReinTechTimeManagement/src/java/employee/data/EmployeeDB.java
@@ -30,29 +30,35 @@ public class EmployeeDB {
     public static Employee selectEmployee(int employeeID){
 
     }
+  
     
-    
-    /*
-    public static ArrayList<User> selectUsers() {
+    //TODO: WEEK 5-6 - get employee array list working
+    public static ArrayList<Employee> selectEmployees(){
+        // work here danny
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
         
-        String query = "SELECT * FROM User";
+        String query = "SELECT * FROM Employee";
         try {
             ps = connection.prepareStatement(query);
             rs = ps.executeQuery();
-            ArrayList<User> users = new ArrayList<User>();
-            while (rs.next())
-            {
-                User user = new User();
-                user.setFirstName(rs.getString("FirstName"));
-                user.setLastName(rs.getString("LastName"));
-                user.setEmail(rs.getString("Email"));
-                users.add(user);
-            }
-            return users;
+            ArrayList<Employee> employeeList = new ArrayList<Employee>();
+                while (rs.next())
+                {
+                    Employee employee = new Employee();
+                    employee.setEmployeeID(rs.getInt("EmloyyeeID"));
+                    employee.setAuthLevel(rs.getInt(query));
+                    employee.setPayRate(rs.getDouble(query));
+                    employee.setFirstName(rs.getString("FirstName"));
+                    employee.setLastName(rs.getString("LastName"));
+                   //employee.setPassword(query);
+                   //employee.setStatus(query);
+                    employeeList.add(employee);
+                }
+            return employeeList ;
+
         } catch (SQLException e) {
             System.out.println(e);
             return null;
@@ -61,11 +67,6 @@ public class EmployeeDB {
             DBUtil.closePreparedStatement(ps);
             pool.freeConnection(connection);
         }
-    */
-    
-    //TODO: WEEK 5-6 - get employee array list working
-    public static ArrayList<Employee> selectEmployees(){
-        
     }
     
     public static ArrayList<TimeClock> selectTimeClocks(){
